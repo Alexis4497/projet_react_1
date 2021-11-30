746c7ba21495b4e10c0eaa10e6fa1e29ae62afea
Alexis Caillé <alexiscaille97@gmail.com>
2021-11-30 16:22:36 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>À propos</h1>
+                <br/>
+                     <p>Aliqua sit Lorem excepteur amet sunt ad enim esse reprehenderit irure nisi nisi ullamco. Enim laboris mollit quis consectetur velit minim elit esse dolor voluptate dolore nisi exercitation deserunt. Sint dolore officia laborum ad et nisi sint incididunt labore amet duis incididunt. Eiusmod labore ipsum minim eu dolore est elit. Exercitation culpa aliqua commodo do et velit.
+
+                Fugiat consequat pariatur eu fugiat officia aute mollit non ipsum veniam quis minim commodo. Sunt id sint nulla nisi pariatur. Sunt nostrud magna voluptate ut ad ipsum aute voluptate irure. Nulla cupidatat proident ex nulla magna nulla minim qui labore Lorem id laboris elit labore. Mollit nisi Lorem nulla excepteur minim aliquip minim.
+
+                Adipisicing pariatur nulla dolore deserunt non. Sunt duis nulla ut minim voluptate sunt nisi in occaecat incididunt ullamco. Adipisicing eu incididunt deserunt culpa sint ad voluptate sint ex laborum consequat laborum. Adipisicing dolor anim non in tempor exercitation id nostrud non aliqua qui deserunt. Velit cupidatat esse enim aute nulla anim duis adipisicing id laborum cillum do id consequat. Duis ad irure aliquip adipisicing ullamco voluptate.
+
+                Consequat commodo in anim anim magna in nulla dolor. Sunt sunt id esse in consequat consectetur et. Minim veniam commodo quis est elit. Esse laboris velit consequat reprehenderit tempor culpa ut do dolore laborum cillum ipsum et ullamco.
+
+                Qui duis irure cillum aute est ullamco. Quis fugiat ea tempor esse aliqua ut eiusmod. Eu nisi do nulla consequat ex quis ex id. Incididunt irure incididunt qui laboris enim enim sint fugiat. Labore consectetur enim culpa proident excepteur voluptate sint consequat sit eiusmod. Quis est non quis ipsum exercitation irure sit voluptate ut sint et proident.
+
+                Ex commodo nisi minim do excepteur occaecat in voluptate cillum adipisicing esse. Ex elit nostrud commodo nostrud. Consequat mollit exercitation labore reprehenderit consectetur. Culpa veniam nisi Lorem laboris consectetur consequat deserunt ut. Excepteur eiusmod pariatur sint ut commodo.
+
+                Nisi fugiat quis culpa excepteur exercitation aute. Nisi anim incididunt aute magna dolor non cupidatat. Proident tempor exercitation id pariatur voluptate. Qui nostrud excepteur proident esse proident esse pariatur deserunt ad. Enim aliquip dolor et est sunt mollit quis consequat labore est aliquip. In exercitation excepteur dolore adipisicing nisi sunt dolore labore.
+
+                Nulla qui dolore excepteur ex nisi sint velit irure esse anim elit. Ex aliqua tempor cillum ut et culpa Lorem dolore dolor dolor Lorem. Laborum do cillum quis amet sunt dolore in duis id magna exercitation adipisicing dolor excepteur.
+
+                Elit ullamco ut magna amet proident laboris eu nisi fugiat fugiat ipsum cillum sunt sunt. Enim fugiat consectetur ea ullamco laborum veniam cillum ex occaecat. Proident voluptate quis mollit laborum veniam.
+
+                Tempor exercitation ut enim nostrud ea id veniam laboris minim. Consequat dolor magna quis aliquip adipisicing aliqua. Proident ullamco esse voluptate duis quis irure. Aliquip non esse occaecat elit cupidatat. Fugiat aliquip reprehenderit commodo sint consequat. Anim sit incididunt quis est culpa nisi nulla.
+                    </p>
+                <br/>
+                     <p>Velit labore officia do proident laborum qui ad ex culpa proident sunt ea. Veniam esse esse do proident eu aliqua id incididunt non. Exercitation laborum eu dolore nisi dolore velit ad duis magna aute anim proident ex.
+
+                Velit nulla cillum anim consequat est in esse dolor. Irure sint ut duis adipisicing proident ut quis laborum labore culpa. Sint officia et quis in mollit et eiusmod irure laboris eu.
+
+                Duis consequat sunt labore excepteur. Aliquip duis cupidatat sint nulla laboris consequat do. Nisi velit laboris cillum dolore in non esse nisi occaecat reprehenderit qui fugiat pariatur. Consectetur consectetur voluptate cillum officia amet proident do cillum nulla et proident.
+
+                Sit reprehenderit culpa aliquip nulla exercitation sit cupidatat mollit incididunt nisi adipisicing proident et in. Ex quis voluptate excepteur labore. Nostrud velit duis officia sunt aliqua adipisicing et fugiat in est irure veniam. Dolore dolor cillum culpa sit est incididunt officia fugiat minim minim commodo ullamco consectetur pariatur. Sint laboris Lorem nisi esse sit irure esse consectetur incididunt proident ea. Minim consequat irure in id eu laboris consequat excepteur ullamco amet minim ipsum nulla sit.
+                    </p>
+        </div>
+    );
+};
+
+export default About;
